a47633d3ae3b3c53e6b58dfc97332708b9870af2
hamdane1548 <oussamahamdane80@gmail.com>
2025-12-08 22:22:39 +0100

    create restController and methode for get product

---
+++ inventory-service/src/main/java/net/oussama/inventoryservice/web/ProductRestContoller.java
@@ -0,0 +1,22 @@
+package net.oussama.inventoryservice.web;
+
+import net.oussama.inventoryservice.entites.Product;
+import net.oussama.inventoryservice.repository.ProductRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ProductRestContoller {
+    private ProductRepository  productRepository;
+    public ProductRestContoller(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+    @GetMapping("/product")
+    public List<Product> productList(){
+        return  productRepository.findAll();
+    }
+}
