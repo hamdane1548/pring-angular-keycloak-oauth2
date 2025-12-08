4ff056fc8a5cdc0d93cd44c8f60d9ea62f09924c
hamdane1548 <oussamahamdane80@gmail.com>
2025-12-08 22:25:41 +0100

    methode get produit

--- inventory-service/src/main/java/net/oussama/inventoryservice/web/ProductRestContoller.java
+++ inventory-service/src/main/java/net/oussama/inventoryservice/web/ProductRestContoller.java
@@ -3,6 +3,7 @@ package net.oussama.inventoryservice.web;
 import net.oussama.inventoryservice.entites.Product;
 import net.oussama.inventoryservice.repository.ProductRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,9 @@ public class ProductRestContoller {
     @GetMapping("/product")
     public List<Product> productList(){
         return  productRepository.findAll();
+    }
+    @GetMapping("/product/{id}")
+    public  Product findProductById(@PathVariable String id){
+        return productRepository.findById(id).get();
     }
 }
